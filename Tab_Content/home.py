0569dc1083f57316b541674d6f02e034a3ced78a
sathjay <sa.jayagopi@gmail.com>
2023-09-04 06:00:29 +0530

    Made changes to home.py

--- Tab_Content/home.py
+++ Tab_Content/home.py
@@ -8,7 +8,7 @@ home_LO = html.Div([
 
 
 
-    html.H3(["Welcome to Social-MediaAnaytics.com: "], className='home_title'),
+    html.H3(["Welcome to SMediaAnaytics.com: "], className='home_title'),
     html.P(["This website was created to demonstrate how Python can be used to collect data from social media platforms via APIs, such as those of YouTube and Reddit. It also showcases how this data can be analyzed using ChatGPT and NLP, and subsequently visualized with Plotly and Dash. The code for this website is available on my GitHub page, with the link located in the 'About Me' section. Please navigate to the tabs above to explore further."], className='home_text'),
 
 
